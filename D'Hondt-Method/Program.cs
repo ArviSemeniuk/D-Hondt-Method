4fb770cd3bf2bcedda1f4a42b51b650034faa8b3
Arvaidas Semeniuk <25202328@students.lincoln.ac.uk>
2021-03-02 22:23:33 +0000

    added ComputeResults method (!!!needs some fixes and extra functionality!!!)

--- D'Hondt-Method/Program.cs
+++ D'Hondt-Method/Program.cs
@@ -14,23 +14,48 @@ namespace D_Hondt_Method
         static void ReadFile()
         {
             string line;
-            int lineNumber = 4;
             List<string> electionResults = new List<string>();
 
             StreamReader lines = new StreamReader("Assessment1Data.txt");
 
-            for(int i = 1; i < lineNumber; i++)
+            for (int i = 1; i < 4; i++)
             {
                 lines.ReadLine();
             }
-            while((line = lines.ReadLine()) != null)
+            while ((line = lines.ReadLine()) != null)
             {
                 electionResults.Add(line);
             }
-            foreach(string party in electionResults)
+            ComputeResults(electionResults);
+        }
+
+        static void ComputeResults(List<string> electionResults)
+        {
+            List<int> votes = new List<int>();
+            int seats = 5; // number of seats that are to be allocated 
+
+            foreach (string party in electionResults)
             {
-                Console.WriteLine(party);
+                string[] results = party.Split(',', ';');
+                votes.Add(Int32.Parse(results[1]));
             }
+            //Console.WriteLine(votes[0]);
+            for (int i = 0; i <= seats; i++)
+            {
+                if (votes[0] >= votes[i])
+                {
+                    votes[0] = votes[0] / 2; // 2 needs to be replaced with an int <variable> since it depends on how many seats the party has already got  
+                }
+                else
+                {
+                    votes[i] = votes[i] / 2; // some fixes need to be made here (incorrect division)
+                }
+            }
+            foreach (int vote in votes)
+            {
+                Console.WriteLine(vote);
+            }
+            //Console.WriteLine(votes[0]);
         }
     }
 }
